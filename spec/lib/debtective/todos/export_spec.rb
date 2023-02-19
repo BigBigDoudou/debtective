dedef6c6d7ffa4dad765e1f8ae904f37e4fe2473
Edouard Piron <ed.piron@gmail.com>
2023-02-19 23:21:13 +0100

    add specs

--- spec/lib/debtective/todos/export_spec.rb
+++ spec/lib/debtective/todos/export_spec.rb
@@ -6,13 +6,13 @@ RSpec.describe Debtective::Todos::Export do
   describe ".call" do
     subject(:todos_export) { described_class.new(user_name: user_name, quiet: quiet).call }
 
-    let(:user_name) { nil }
+    let(:user_name) { "Edouard Piron" }
     let(:quiet) { false }
 
     it "exports the todos" do
       location = "spec/dummy/app/controllers/users_controller.rb:4"
       author = "Edouard Piron"
-      days = "9"
+      days = /\d+/
       size = "6"
       expect { todos_export }.to output(/#{location}\s+|\s#{author}\s+|\s#{days}\s+|\s#{size}\s+/).to_stdout
     end
